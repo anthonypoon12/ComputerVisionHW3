fd9ebb37570e5326f1be431e4df730425270089a
anthonypoon12 <anthony@poons.us>
2024-03-16 16:44:01 -0400

    attempt at h1

--- h1.cc
+++ h1.cc
@@ -1,5 +1,5 @@
 //
-// <YOUR NAME>
+// Anthony Poon
 // Computational Vision Homework 3
 // Program 1
 //
@@ -10,20 +10,75 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 using namespace ComputerVisionProjects;
 
+bool isInBounds(Image image, int row, int col) {
+  // Number of rows and columns in the image
+  size_t numRows = image.num_rows();
+  size_t numCols = image.num_columns();
 
+  if (row < 0 || row >= numRows)
+    return false;
+  if (col < 0 || col >= numCols)
+    return false;
+  return true;
+}
+
+int applyFilter(Image image, int row, int col, vector<vector<int>> filter) {
+  int sum = 0;
+  for (int i = 0; i < filter.size(); i++) {
+    for (int j = 0; j < filter[i].size(); j++) {
+      int newRow = row + (i - filter.size() / 2);
+      int newCol = col + (j - filter[i].size() / 2);
+      if (isInBounds(image, newRow, newCol))
+        sum += image.GetPixel(newRow, newCol) * filter[i][j];
+    }
+  }
+  return sum;
+}
 
  // @brief Implementation of Task Program 1 to produce an edge gray-level image
  //  
  // @param input_filename the name of the input image
  // @param output_filename the name of the gray-level output edge image
 void ComputeEdgeImage(const string &input_filename, const string &output_filename) {
-  cout << "Producing Edge Image" << endl;
-  cout << "Input filename: " << input_filename << endl;
-  cout << "Output filename: " << output_filename << endl;
+  Image image;
+  if (!ReadImage(input_filename, &image)) {
+    cout <<"Can't open file " << input_filename << endl;
+    return;
+  }
+
+  Image newImage = image;
+
+  // Number of rows and columns in the image
+  size_t numRows = image.num_rows();
+  size_t numCols = image.num_columns();
+  vector<vector<int>> filter1 = {
+    {-1, 0, 1},
+    {-2, 0, 2},
+    {-1, 0, 1}
+  };
+  vector<vector<int>> filter2 = {
+    {1, 2, 1},
+    {0, 0, 0},
+    {-1, -2, -1}
+  };
+
+  for (int i = 0; i < numRows; i++) {
+    for (int j = 0; j < numCols; j++) {
+      int newValue1 = applyFilter(image, i, j, filter1);
+      int newValue2 = applyFilter(image, i, j, filter2);
+      newImage.SetPixel(i, j, newValue1*newValue1 + newValue2*newValue2);
+      cout << i <<  " " << j << endl;
+    }
+  }
+
+  if (!WriteImage(output_filename, newImage)){
+    cout << "Can't write to file " << output_filename << endl;
+  }
 }
 
 int main(int argc, char **argv){  
