aebabed780b677c2480ef833e23ba1d67486d001
anthonypoon12 <anthony@poons.us>
2024-03-18 00:12:21 -0400

    h3 fixed i think

--- h3.cc
+++ h3.cc
@@ -1,5 +1,5 @@
 //
-// <YOUR NAME>
+// Anthony Poon
 // Computational Vision Homework 3
 // Program 3
 //
@@ -65,27 +65,26 @@ void ComputeHoughTransform(const string &input_filename, const string & output_h
   size_t numCols = image.num_columns();
   
   const int rhoRange = sqrt(numRows*numRows + numCols*numCols);
-  const int thetaWidth = rhoRange;
+  // const int thetaWidth = rhoRange;
+  const int thetaWidth = 181;
+  // const int arrWidth = rhoRange;
+  const int arrWidth = 1600;
 
-  vector<vector<int>> arr(thetaWidth, vector<int>(rhoRange, 0));
+  vector<vector<int>> arr(thetaWidth, vector<int>(arrWidth, 0));
   int maximum = 0;
 
   for (int i = 0; i < numRows; i++) {
     for (int j = 0; j < numCols; j++) {
       double color = image.GetPixel(i, j);
       if (color > 0) {
-
-        // max of theta is pi
-        // max of rho is diagonal
-        int x = i - numRows / 2;
-        int y = j - numCols / 2;
-        
         for (int z = 0; z < thetaWidth; z++) {
           double theta = M_PI * z / thetaWidth;
-          double rho = x*cos(theta) + y*sin(theta);
-          rho = rho + rhoRange / 2;
-          arr[z][int(rho)]++;
-          maximum = max(maximum, arr[z][int(rho)]);
+          double rho = i*cos(theta) + j*sin(theta);
+          int k = int(rho/rhoRange * arrWidth);
+          if (k >= 0 && k < arrWidth){
+            arr[z][k]++;
+            maximum = max(maximum, arr[z][k]);
+          }
         }
       }
     }
@@ -93,14 +92,15 @@ void ComputeHoughTransform(const string &input_filename, const string & output_h
 
 
   Image hough_image;
-  hough_image.AllocateSpaceAndSetSize(thetaWidth,rhoRange);
+  hough_image.AllocateSpaceAndSetSize(thetaWidth,arrWidth);
   hough_image.SetNumberGrayLevels(255);
 
   vector<vector<int>> newArr(thetaWidth/3, vector<int>(rhoRange/3, 0));
 
   for (int i = 0; i < thetaWidth; i++) {
-    for (int j = 0; j < rhoRange; j++) {
-      setSquare(&hough_image, i, j, arr[i][j] * 255/maximum, 10);
+    for (int j = 0; j < arrWidth; j++) {
+      setSquare(&hough_image, i, j, arr[i][j] * 255/maximum, 3);
+      // hough_image.SetPixel(i, j, arr[i][j] * 255/maximum);
       if (i/3 < newArr.size() && j/3 < newArr[0].size())
         newArr[i/3][j/3] = hough_image.GetPixel(i, j);
     }
